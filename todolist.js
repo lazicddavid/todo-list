5050da7332681fd29223497916d73bd70adb4484
David Lazic <lazic.ddavid@gmail.com>
2025-10-14 14:45:23 +0200

    Added console.log

---
+++ todolist.js
@@ -0,0 +1,16 @@
+/* 
+const DOMElements = {
+  buttonNewBook = document.getElementById("btnNewBook");
+}
+const bookManager = {
+  library: [],
+  addBook(book) {
+    this.library.push(book)
+  }
+} 
+
+const inputManager = {
+  titleValue: "";
+}
+*/
+console.log(--------------------)
