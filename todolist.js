a7e6657aba51f7d70507a6f6b3e24f5cbe1c81d7
David Lazic <lazic.ddavid@gmail.com>
2025-10-18 22:07:52 +0200

    added guess type off

--- todolist.js
+++ todolist.js
@@ -104,5 +104,9 @@ function pozdravi() {
 document.querySelector('.check').add ('click' function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
+  
+  if (!guess) {
+    document.querySelector('.message').textContent 'no number';
+  }
 }
 )
